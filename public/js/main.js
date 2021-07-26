7cacdddb9ee80e0a9a5655502c291024d616beb9
lucas.j <lucas.j@hpcnt.com>
2021-07-27 02:04:01 +0900

    js arrow up

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
-import "./utils/scroll.js";
+import {
+  handleScroll,
+  handleScrollIntoView,
+  handleFade,
+  handleShow,
+} from "./utils/scroll.js";
+
+window.addEventListener("scroll", () => {
+  handleScroll();
+  handleFade();
+  handleShow();
+});
+
+document //
+  .querySelector(".navbar__menu")
+  .addEventListener("click", (event) => {
+    handleScrollIntoView(event.target.dataset.link);
+  });
+
+document //
+  .querySelector(".home__contact").onclick = () => {
+  handleScrollIntoView("#contact");
+};
+
+document.querySelector(".arrow-up").onclick = () => {
+  handleScrollIntoView("#home");
+};
--- public/js/utils/scroll.js
+++ public/js/utils/scroll.js
@@ -2,12 +2,39 @@
 
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-window.addEventListener("scroll", () => {
-  setTimeout(() => {
-    if (window.scrollY > navbarHeight) {
-      navbar.classList.add("navbar--dark");
-    } else {
-      navbar.classList.remove("navbar--dark");
-    }
-  }, 100);
-});
+export const handleScroll = () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+};
+//
+export const handleScrollIntoView = (target) => {
+  if (!target) return;
+
+  const targetEl = document.querySelector(target);
+  targetEl.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+//
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+
+export const handleFade = () => {
+  const opacity = 1 - window.scrollY / homeHeight;
+  if (opacity < 1 && opacity > 0) {
+    home.style.opacity = opacity;
+  }
+};
+
+//
+const arrow = document.querySelector(".arrow-up");
+export const handleShow = () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrow.classList.add("visible");
+  } else {
+    arrow.classList.remove("visible");
+  }
+};
